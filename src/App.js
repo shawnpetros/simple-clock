b53a72a53f7968edc0dc9cd9edfd9c5960cdfeba
Shawn Petros <shawnpetros@northwesternmutual.com>
2018-12-17 12:40:00 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  state = {
+    time: this.formatTime(new Date(Date.now()))
+  }
+
+  componentWillMount () {
+    this.intervalId = setInterval(() => this.tick(), 1000)
+  }
+
+  formatTime (date) {
+    const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+    const mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+    const secs = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+
+    return `${hours}:${mins}:${secs}`
+  }
+
+  tick () {
+    this.setState({
+      time: this.formatTime(new Date(Date.now()))
+    })
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.intervalId)
+  }
+
+  render () {
+    const { time } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='App'>
+        <header className='App-header'>
+          { time }
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
