f112b09db3cfeced859472c3f164339c46b0c2cc
Shawn Petros <shawnpetros@northwesternmutual.com>
2019-06-06 09:36:02 -0500

    year countdown

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import './App.css'
 class App extends Component {
   state = {
     time: this.formatTime(new Date(Date.now())),
-    countDownDate: new Date('May 22, 2019 13:00:00').getTime(),
+    countDownDate: new Date('December 31, 2019 11:59:59').getTime(),
     countDown: 0
   }
 
   breakdownTime () {
-    const distance = this.state.countDownDate - new Date().getTime();
+    const distance = this.state.countDownDate - new Date().getTime()
 
     if (distance <= 0) return 'EXPIRED!!!'
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    )
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
     return `${days}d ${hours}h ${minutes}m ${seconds}s`
   }
@@ -26,8 +28,10 @@ class App extends Component {
 
   formatTime (date) {
     const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
-    const mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
-    const secs = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+    const mins =
+      date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+    const secs =
+      date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
 
     return `${hours}:${mins}:${secs}`
   }
@@ -44,13 +48,13 @@ class App extends Component {
   }
 
   render () {
-    const { time } = this.state
+    const { countDown } = this.state
 
     return (
       <div className='App'>
         <header className='App-header'>
-          { time }
-          {/* { countDown } */}
+          {/* { time } */}
+          {countDown}
         </header>
       </div>
     )
